004d6c379bee9d5e68bc68938f16446fffa89ea0
Comp <ovojan84@gmail.com>
2023-04-08 12:13:30 +0400

    DB

--- src/main/java/model/Employee.java
+++ src/main/java/model/Employee.java
@@ -5,13 +5,14 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
-public class Company {
+@AllArgsConstructor
+public class Employee {
     private int id;
     private String name;
-    private String country;
-
+    private String surname;
+    private String email;
+    private int companId;
 
 
 }
